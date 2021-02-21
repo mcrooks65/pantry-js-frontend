3f87da279f0274f27482ceadf04d99e5e66d2fcc
mcrooks65 <mfcrooks@gmail.com>
2021-02-20 16:52:21 -0800

    Create getFoods() in index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 console.log("index.html file is hooked into index.js!!!")
+
+const endPoint = "http://127.0.0.1:3000/api/v1/foods"
+
+document.addEventListener('DOMContentLoaded', () => {
+    getFoods();
+})
+
+function getFoods() {
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(foods => {
+        console.log(foods);
+        foods.data.forEach(food =>{           
+        const foodMarkup = `
+        <div data-id=${food.id}>
+            <h2>${food.attributes.name}</h2>
+            <h3>Quantity: ${food.attributes.quantity}</h3>
+            <p>${food.attributes.category}</p>
+            <button data-id=${food.id}>edit</button>
+        </div>
+        <br>`;
+        document.querySelector('#food-container').innerHTML += foodMarkup
+        });
+    })
+}
