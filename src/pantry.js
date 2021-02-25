f55d288732d8ab6358af2f558ada6c57e29e711d
mcrooks65 <mfcrooks@gmail.com>
2021-02-24 17:24:19 -0800

    Build constructor for Pantry class

--- src/pantry.js
+++ src/pantry.js
@@ -1,1 +1,12 @@
 console.log("pantry.js file is hooked into index.html!!!")
+
+class Pantry {
+    constructor(pantry, pantryAttributes) {
+        this.id = pantry.id;
+        this.name = pantryAttributes.name;
+        this.foods = pantryAttributes.foods;
+        Pantry.all.push(this);
+    }
+}
+
+Pantry.all = [];
